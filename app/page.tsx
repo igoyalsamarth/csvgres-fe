f99db04ba940cd9a7bd337ebdf6633f11b132f22
Samarth Goyal <samarth.goyal1999@gmail.com>
2025-02-06 18:59:09 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-2">
+      <Button>Click me</Button>
+    </div>
+  );
+}
